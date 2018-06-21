8ac367d400fb31031ff198f98d073a212ea9dc93
JQuinnie <jenn8866@gmail.com>
2018-06-20 22:09:10 -0600

    Introduce component

---
+++ src/components/QuoteBox.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class QuoteBox extends React.Component {
+
+  render() {
+    return (
+      <div class="container">
+        <div class="row">
+          <div className="col">
+            <h1>This is the container</h1>
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+}
+
+export default QuoteBox;
