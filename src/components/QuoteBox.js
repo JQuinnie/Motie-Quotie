71bab56bab0c127c4fa4f9a7cb6bc9fff9dc5015
JQuinnie <jenn8866@gmail.com>
2018-06-21 16:52:33 -0600

    Update with sample API call

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -2,12 +2,37 @@ import React from 'react';
 
 class QuoteBox extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      email: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://randomuser.me/api/?results=10')
+      .then(results => {
+        return results.json();
+      }).then(data => {
+        let email = data.results.map((mail) => {
+          return (
+            <div key={mail.results}>
+              {mail.email}
+            </div>
+          )
+        })
+        this.setState({ email: email })
+        console.log('state', this.state.email)
+      })
+  }
+
   render() {
     return (
       <div class="container">
         <div class="row">
           <div className="col">
-            <h1>This is the container</h1>
+            <h1>This is the container to test quotes</h1>
+            <p>{this.state.email}</p>
           </div>
         </div>
       </div>
