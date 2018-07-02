11df45d324904203f40a73742b9707410f492bc0
JQuinnie <jenn8866@gmail.com>
2018-07-01 22:33:14 -0600

    Remove state from component, pass prop to component instead

---
+++ src/quoteDex.js
@@ -0,0 +1,28 @@
+let quoteCollection = {
+  1: {
+    quote: "What you get by achieving your goals is not as important as what you become by achieving your goals",
+    author: "Henry David Thoreau"
+  },
+  2: {
+    quote: "Talent hits a target no one else can hit; Genius hits a target no one else can see",
+    author: "Arthur Schopenhauer"
+  },
+  3: {
+    quote: "The grass is greener where you water it",
+    author: "Neil Barringham"
+  },
+  4: {
+    quote: "Keep your face always toward the sunshine - and shadows will fall behind you",
+    author: "Walt Whitman"
+  },
+  5: {
+    quote: "Try to be a rainbow in someone's cloud",
+    author: "Maya Angelou"
+  },
+  6: {
+    quote: "Happiness is not something you postpone for the future; it is something you design for the present",
+    author: "Jim Rohn"
+  }
+}
+
+export default quoteCollection;
